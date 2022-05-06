6b83e8ec881244e137c9867551081fc6ad9641f3
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-05-06 23:15:48 +0300

    Feat : Code refactoring

--- app/models.py
+++ app/models.py
@@ -51,16 +51,14 @@ class Review:
         return response
 
 class User(UserMixin,db.Model):
+
     __tablename__ = 'users'
 
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255),index = True)
-    email = db.Column(db.String(255),unique = True,index = True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
-    bio = db.Column(db.String(255))
-    profile_pic_path = db.Column(db.String())
+    email = db.Column(db.String(255),unique = True,index = True)
     password_hash = db.Column(db.String(255))
-    pass_secure = db.Column(db.String(255))
 
     @property
     def password(self):
@@ -68,10 +66,10 @@ class User(UserMixin,db.Model):
 
     @password.setter
     def password(self, password):
-        self.pass_secure = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password)
 
     def verify_password(self,password):
-        return check_password_hash(self.pass_secure,password)
+        return check_password_hash(self.password_hash,password)
 
     def __repr__(self):
         return f'User {self.username}'
@@ -82,7 +80,5 @@ class Role(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     name = db.Column(db.String(255))
     users = db.relationship('User',backref = 'role',lazy="dynamic")
-
     def __repr__(self):
         return f'User {self.name}'
-
